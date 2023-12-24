544d945a17f35d6943170488c00da5db914bf9a5
Lucas Mota <contato.lukamota@gmail.com>
2023-12-24 15:28:28 -0300

    update home screen and apply SEO for the website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 
 export function Home() {
+  const date = new Date()
+  const currentYear = date.getFullYear()
   return (
     <div className="mx-auto max-w-3xl text-center">
       <div className="flex justify-center items-center gap-4">
@@ -13,7 +15,7 @@ export function Home() {
       </div>
 
       <h2 className="mx-auto mt-4 max-w-4xl sm:text-xl/relaxed">
-        <span className="font-bold italic">Toolbox</span>, como o próprio nome sugere, é uma <span className="font-bold underline underline-offset-4">caixa de ferramentas</span>.
+        <span className="font-bold italic">Toolbox</span>, como o próprio nome diz, é uma: <span className="font-bold underline underline-offset-4">caixa de ferramentas</span>.
       </h2>
 
       <p className="mx-auto mt-4 max-w-4xl sm:text-xl/relaxed">
@@ -26,31 +28,36 @@ export function Home() {
           className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
           to="/conversor-de-textos"
         >
-          Conversor de Textos
+          📝 Conversor de Textos
         </NavLink>
 
         <NavLink
           className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
           to="/criptografar-arquivos"
         >
-          Criptografar Arquivos
+          🔐 Criptografar Arquivos
         </NavLink>
 
         <NavLink
           className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
           to="/validador-de-cpf"
         >
-          Validador de CPF
+          ✅ Validador de CPF
         </NavLink>
 
         <NavLink
           className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
           to="/validador-de-cnpj"
         >
-          Validador de CNPJ
+          ☑️ Validador de CNPJ
         </NavLink>
 
       </div>
-    </div>
+
+      <footer className="mx-auto mt-20 max-w-4xl text-base sm:text-base/relaxed">
+        <p>🧰 Toolbox - {currentYear}</p>
+        <p>💻 Criado e desenvolvido por: <a href="https://github.com/lmdvlpr" target="_blank" rel="noopener noreferrer"><span className="font-bold underline underline-offset-4">Lucas Mota</span></a></p>
+      </footer>
+    </div >
   )
 }
