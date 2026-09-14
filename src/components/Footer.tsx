1d097abbc9b726ea2b2e2d5a5dcb439fb5caaca1
Lucas Mota <contato.lukamota@gmail.com>
2026-09-14 12:56:06 -0300

    feat: add dynamic footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { Separator } from '@/components/ui/separator'
 
 export function Footer() {
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className="mx-auto w-full max-w-7xl px-4 pb-8 pt-4 text-sm text-muted-foreground sm:px-6 lg:px-8">
       <Separator className="mb-6" />
       <p className="text-center">
-        Criado e desenvolvido por{' '}
+        {currentYear} - Criado e desenvolvido por:{' '}
         <a
           href="https://www.lmdvlpr.com/"
           target="_blank"
